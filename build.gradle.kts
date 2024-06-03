2c381ea2101c6d77e397c4317c5aae8ae3d9c164
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-06-04 00:03:35 +0900

    feat: build.gradle追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
+	id("org.openapi.generator") version "7.6.0"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
 }
@@ -33,6 +35,9 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
+	implementation("io.swagger.core.v3:swagger-annotations:2.2.22")
+	implementation("org.springframework.boot:spring-boot-starter-validation:3.3.0")
 }
 
 tasks.withType<KotlinCompile> {
@@ -44,4 +49,32 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.register<GenerateTask>("buildApiDoc") {
+	generatorName.set("html2")
+	inputSpec.set("$rootDir/src/main/resources/openapi.yaml")
+	outputDir.set("$buildDir/api-doc")
+}
+
+openApiGenerate {
+	generatorName.set("kotlin-spring")
+	inputSpec.set("$rootDir/src/main/resources/openapi.yaml")
+	outputDir.set("$buildDir/spring")
+	apiPackage.set("com.example.todoapi.controller")
+	modelPackage.set("com.example.todoapi.model")
+	configOptions.set(
+		mapOf(
+			"interfaceOnly" to "true",
+			"useSpringBoot3" to "true"
+		)
+	)
+}
+
+sourceSets {
+	main {
+		java {
+			srcDirs("$buildDir/spring/src/main/kotlin")
+		}
+	}
 }
