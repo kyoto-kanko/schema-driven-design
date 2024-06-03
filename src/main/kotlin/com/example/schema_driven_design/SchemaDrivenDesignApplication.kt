672092c77ba3fe0b7c2bb2378842979ebfe1e46c
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-06-03 23:31:16 +0900

    first commit

---
+++ src/main/kotlin/com/example/schema_driven_design/SchemaDrivenDesignApplication.kt
@@ -0,0 +1,11 @@
+package com.example.schema_driven_design
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchemaDrivenDesignApplication
+
+fun main(args: Array<String>) {
+	runApplication<SchemaDrivenDesignApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/schema_driven_design/SchemaDrivenDesignApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.schema_driven_design
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchemaDrivenDesignApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
