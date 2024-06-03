da1c55d8d0828be4f14893bf717be9841e5b8f28
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-06-04 00:04:48 +0900

    refactor: コードフォーマット

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,79 +2,79 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-	id("org.springframework.boot") version "3.3.0"
-	id("io.spring.dependency-management") version "1.1.5"
-	id("org.openapi.generator") version "7.6.0"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+    id("org.openapi.generator") version "7.6.0"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
-	implementation("io.swagger.core.v3:swagger-annotations:2.2.22")
-	implementation("org.springframework.boot:spring-boot-starter-validation:3.3.0")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.22")
+    implementation("org.springframework.boot:spring-boot-starter-validation:3.3.0")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.register<GenerateTask>("buildApiDoc") {
-	generatorName.set("html2")
-	inputSpec.set("$rootDir/src/main/resources/openapi.yaml")
-	outputDir.set("$buildDir/api-doc")
+    generatorName.set("html2")
+    inputSpec.set("$rootDir/src/main/resources/openapi.yaml")
+    outputDir.set("$buildDir/api-doc")
 }
 
 openApiGenerate {
-	generatorName.set("kotlin-spring")
-	inputSpec.set("$rootDir/src/main/resources/openapi.yaml")
-	outputDir.set("$buildDir/spring")
-	apiPackage.set("com.example.todoapi.controller")
-	modelPackage.set("com.example.todoapi.model")
-	configOptions.set(
-		mapOf(
-			"interfaceOnly" to "true",
-			"useSpringBoot3" to "true"
-		)
-	)
+    generatorName.set("kotlin-spring")
+    inputSpec.set("$rootDir/src/main/resources/openapi.yaml")
+    outputDir.set("$buildDir/spring")
+    apiPackage.set("com.example.todoapi.controller")
+    modelPackage.set("com.example.todoapi.model")
+    configOptions.set(
+        mapOf(
+            "interfaceOnly" to "true",
+            "useSpringBoot3" to "true"
+        )
+    )
 }
 
 sourceSets {
-	main {
-		java {
-			srcDirs("$buildDir/spring/src/main/kotlin")
-		}
-	}
+    main {
+        java {
+            srcDirs("$buildDir/spring/src/main/kotlin")
+        }
+    }
 }
